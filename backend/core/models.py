1bc4ee3b57890a97d8e7f1681c265a7de035df06
Aditya <adityasanandan@gmail.com>
2024-02-21 21:41:15 -0600

    New backend stuff by @adityaanandan

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+    title = models.TextField()
+    topic = models.CharField(max_length=50)
+    id = models.CharField(max_length=50)
+    validity = models.CharField(max_length=50)
+
+
+
+     
+
+
+
