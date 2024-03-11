3b4623b0850038e7578fdd18c79ed4f383f40ef5
Pranav Poddutoori <pranav.poddutoori@gmail.com>
2024-03-11 03:22:17 -0500

    "Made Acceptable/Unacceptable Outputs colorful"

--- app/components/Row.tsx
+++ app/components/Row.tsx
@@ -1,5 +1,6 @@
 import { testType } from "@/lib/Types";
 import Buttons from "@/app/components/Buttons";
+import {CiCircleCheck, CiCircleRemove} from "react-icons/ci";
 
 type rowProps = {
   test: testType,
@@ -10,10 +11,24 @@ const Row = ({ test } : rowProps) => {
     <div className={'border-gray-500 border-b-2 w-full min-h-16 items-center flex justify-between'}>
       <div className={'text-md font-light w-[60%]'}>{test.title}</div>
       <div className={'ml-auto flex w-[40%] justify-between'}>
-        <div className={'text-xl w-[50%] flex justify-center text-center font-light'}>{test.label}</div>
-        <div className={'w-[40%]'}>
-          <Buttons test={test}/>
-        </div>
+          {
+              test.label == "acceptable" || test.label == "Acceptable" ?
+                  <div className={'w-[50%] flex justify-center'}>
+                      <div className={'bg-green-50 text-green-500 rounded-md text-xl text-center ' +
+                          'flex justify-left font-light border border-green-500 pr-1'}>
+                          <CiCircleCheck className={'h-6 w-6 pt-1 text-green-500'}/>Acceptable
+                      </div>
+                  </div>:
+                  <div className={'w-[50%] flex justify-center'}>
+                      <div className={'bg-red-50 text-red-500 rounded-md text-xl text-center ' +
+                          'flex justify-left font-light border border-red-500 pr-1'}>
+                        <CiCircleRemove className={'h-6 w-6 pt-1 text-red-500'}/> Unacceptable
+                      </div>
+                  </div>
+          }
+          <div className={'w-[40%]'}>
+              <Buttons test={test}/>
+          </div>
       </div>
     </div>
   )
--- app/components/TaskGraph.tsx
+++ app/components/TaskGraph.tsx
@@ -1,22 +1,84 @@
-import Histogram from "@/app/components/Histogram";
-import {BarChart} from "@mui/x-charts";
-import React from "react";
+import React, {useContext} from "react";
+import { Bar } from 'react-chartjs-2';
+import { TestDecisionsContext } from "@/lib/TestContext";
+
+import {
+    Chart as ChartJS,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Tooltip,
+    Legend
+} from 'chart.js';
+
+ChartJS.register(BarElement,
+    CategoryScale,
+    LinearScale,
+    Tooltip,
+    Legend);
 
 const TaskGraph = () => {
+    const {testDecisions, currentTopic} = useContext(TestDecisionsContext);
+    const restPE = testDecisions.PE.denied.length + testDecisions.PE.trashed.length;
+    const restKE = testDecisions.KE.denied.length + testDecisions.KE.trashed.length;
+    const restLCE = testDecisions.LCE.denied.length + testDecisions.LCE.trashed.length;
+
+    const data_topic = {
+        labels: ['PE', 'KE', 'LCE'],
+        datasets: [{
+            label: 'Matching Your Evaluation',
+            data: [testDecisions.PE.approved.length, testDecisions.KE.approved.length, testDecisions.LCE.approved.length],
+            backgroundColor: '#FFE58F'
+        },
+        {
+            label: 'Not Matching Your Evaluation',
+            data: [restPE, restKE, restLCE],
+            backgroundColor: '#1AA367'
+        }]
+    };
+
+    const data_acceptable = {
+        labels: ['A', 'U', ''],
+        datasets: [{
+            label: 'Matching Your Evaluation',
+            data: [20, 50],
+            backgroundColor: '#FFE58F'
+        },
+        {
+            label: 'Not Matching Your Evaluation',
+            data: [60, 30],
+            backgroundColor: '#1AA367'
+        }]
+    };
+
+    const options = {
+        indexAxis: 'y',
+        scales: {
+          x: {
+            stacked: true,
+            grace: 1,
+          },
+          y: {
+            beginAtZero: true,
+            stacked: true,
+          }
+        },
+        plugins: {
+            legend: {
+                display: true,
+                position: "bottom",
+                align: "start"
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: true
+    };
+
     return (
-        <div className={'float-end border-gray-600 w-full h-3/4 justify-center items-center flex flex-col'}>
-            <h1>Task Graph</h1>
-            <BarChart
-            xAxis={[{
-              scaleType: 'band',
-              data: ['Tests'],
-              labelStyle: {fill: 'black', fontSize: 16, fontWeight: 500},
-            }]}
-            series={[{data: [47], label: 'Accepted'}, {data: [53], label: 'Rejected'}]} // Combine all data into a single series
-            margin={{top: 30, right: 30, bottom: 50, left: 30}}
-            axisHighlight={{x: 'none', y: 'none'}}
-            colors={['#00FF00', '#FF0000']}
-            />
+        <div className={'float-end border-gray-600 w-full h-3/4 justify-start items-center flex flex-col'}>
+            <h2 className={'align-middle'}> Visualization </h2>
+            <Bar data={data_topic} options={options}> </Bar>
+            <Bar data={data_acceptable} options={options}> </Bar>
         </div>
     )
 }
