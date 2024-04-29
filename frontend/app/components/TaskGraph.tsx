adc450f88553c7c44074380ef7b76a96fa036381
Pranav <ppoddutoori@wisc.edu>
2024-04-29 13:29:17 -0500

    somem changes

--- frontend/app/components/TaskGraph.tsx
+++ frontend/app/components/TaskGraph.tsx
@@ -23,65 +23,89 @@ ChartJS.register(BarElement,
     Legend,
     Title);
 
-const TaskGraph = () => {
+type taskGraphProps = {
+    isPerturbed: boolean,
+}
+const TaskGraph = ({isPerturbed}: taskGraphProps) => {
     const {testData} = useContext(TestDataContext);
     // Currently selected Perturbation from the filter used in the last graph
     const [selectedPerturbation, setSelectedPerturbation] = useState<string>('spelling'); // New state
-
-    // Grabs all tests from the test data
+    const [topicData, setTopicData] = useState<any>({}); // New state
+    const [pertData, setPertData] = useState<any>({}); // New state
+    const [criteriaData, setCriteriaData] = useState<any>({}); // New state
+    const [totalTests, setTotalTests] = useState<number>(0); // New state
+    
     const tests = testData.tests.PE.concat(testData.tests.KE, testData.tests.LCE);
     const decisionValidity = ['approved', 'denied'];
     const topicTypes = ['PE', 'KE', 'LCE'];
-
-    // Gets all data for topic graph
-    let topicData: any = {};
-    decisionValidity.forEach(validity => {
-        topicData[validity] = {};
-        topicTypes.forEach(type => {
-            topicData[validity][type] = tests.filter((test: testType) => test.topic === type && test.validity === validity);
-        });
-    });
+    
+    // Use effect that updates topic data when testData.tests are updated -> sets data for topic graph
+    useEffect(() => {
+        async function fetchTests() {
+            let tests: any = {};
+            decisionValidity.forEach(validity => {
+                tests[validity] = {};
+                topicTypes.forEach(type => {
+                    tests[validity][type] = tests.filter((test: testType) => test.topic === type && test.validity === validity);
+                });
+            });
+            setTopicData(tests);
+        }
+        fetchTests();
+    }, [testData.tests, setTopicData]);
 
     // Sets the data for the tests that are graded by topic
     const data_topic = {
         labels: topicTypes,
         datasets: [{
             label: 'Matching Your Evaluation',
-            data: [topicData[], testData.test_decisions.KE.approved.length, testData.test_decisions.LCE.approved.length],
+            data: [topicData[decisionValidity[0]][topicTypes[0]].length, topicData[decisionValidity[0]][topicTypes[1]].length,
+                topicData[decisionValidity[0]][topicTypes[2]].length],
             backgroundColor: '#52C41A'
         },
         {
             label: 'Not Matching Your Evaluation',
-            data: [testData.test_decisions.PE.denied.length, testData.test_decisions.KE.denied.length, testData.test_decisions.LCE.denied.length],
+            data: [topicData[decisionValidity[1]][topicTypes[0]].length, topicData[decisionValidity[1]][topicTypes[1]].length,
+                topicData[decisionValidity[1]][topicTypes[2]].length],
             backgroundColor: '#FF4D4F'
         }]
     };
 
+    // useEffect that updates pertData when testData.tests are updated -> sets data for perturbation graph
+    const decisionTypes = ['Base', 'Spelling', 'Negation', 'Synonyms', 'Paraphrase', 'Acronyms', 'Antonyms', 'Spanish'];
+    useEffect(() => {
+        async function fetchPerts() {
+            const perts: any = {};
+            decisionValidity.forEach(validity => {
+                perts[validity] = {};
+                decisionTypes.forEach(type => {
+                    if(type === 'Base') {
+                        perts[validity][type] = tests.filter((test: testType) => test.validity === validity);
+                    }
+                    perts[validity][type] = testData.pert_decisions[validity].filter((pert: any) => pert.type.toLowerCase() === type.toLowerCase());
+                });
+            });
+            setPertData(perts);
+        }
+        fetchPerts();
+    }, [testData, setPertData, pertData]);
     // Sets the data for the graph for all the perturbations graded by type
-    const decisionTypes = ['Spelling', 'Negation', 'Synonyms', 'Paraphrase', 'Acronyms', 'Antonyms', 'Spanish'];
-    const pert_data: any = {};
-    decisionValidity.forEach(validity => {
-        pert_data[validity] = {};
-        decisionTypes.forEach(type => {
-            pert_data[validity][type] = testData.pert_decisions[validity].filter((pert: any) => pert.type.toLowerCase() === type.toLowerCase());
-        });
-    });
 
     const data_perturbations = {
-        labels: ["Base", ...decisionTypes],
+        labels: decisionTypes,
         datasets: [{
             label: 'Matching Your Evaluation',
-            data: [totalApproved, pert_data[decisionValidity[0]][decisionTypes[1]].length, pert_data[decisionValidity[0]][decisionTypes[2]].length,
-                pert_data[decisionValidity[0]][decisionTypes[3]].length, pert_data[decisionValidity[0]][decisionTypes[4]].length,
-                pert_data[decisionValidity[0]][decisionTypes[5]].length, pert_data[decisionValidity[0]][decisionTypes[6]].length],
+            data: [pertData[decisionValidity[0]][decisionTypes[0]], pertData[decisionValidity[0]][decisionTypes[1]].length, pertData[decisionValidity[0]][decisionTypes[2]].length,
+                pertData[decisionValidity[0]][decisionTypes[3]].length, pertData[decisionValidity[0]][decisionTypes[4]].length,
+                pertData[decisionValidity[0]][decisionTypes[5]].length, pertData[decisionValidity[0]][decisionTypes[6]].length],
             backgroundColor: '#52C41A'
         },
         {
             label: 'Not Matching Your Evaluation',
-            data: [totalDenied, pert_data[decisionValidity[1]][decisionTypes[1]].length,
-                pert_data[decisionValidity[1]][decisionTypes[2]].length, pert_data[decisionValidity[1]][decisionTypes[3]].length,
-                pert_data[decisionValidity[1]][decisionTypes[4]].length, pert_data[decisionValidity[1]][decisionTypes[5]].length,
-                pert_data[decisionValidity[1]][decisionTypes[6]].length],
+            data: [pertData[decisionValidity[1]][decisionTypes[0]], pertData[decisionValidity[1]][decisionTypes[1]].length,
+                pertData[decisionValidity[1]][decisionTypes[2]].length, pertData[decisionValidity[1]][decisionTypes[3]].length,
+                pertData[decisionValidity[1]][decisionTypes[4]].length, pertData[decisionValidity[1]][decisionTypes[5]].length,
+                pertData[decisionValidity[1]][decisionTypes[6]].length],
             backgroundColor: '#FF4D4F'
         }]
     };
@@ -93,10 +117,13 @@ const TaskGraph = () => {
             criteria_data[validity] = {};
             decisionLabels.forEach(label => {
                 if(selectedPerturbation === 'base') {
-                    return;
+                    criteria_data[validity][label] = tests.filter((test: testType) =>
+                        test.validity.toLowerCase() === validity && test.label.toLowerCase() === label
+                    );
+                } else {
+                     criteria_data[validity][label] = testData.pert_decisions[validity].filter((pert: any) =>
+                    pert.label.toLowerCase() === label.toLowerCase() && pert.type.toLowerCase() === selectedPerturbation.toLowerCase());
                 }
-                criteria_data[validity][label] = testData.pert_decisions[validity].filter((pert: any) =>
-                pert.label.toLowerCase() === label.toLowerCase() && pert.type.toLowerCase() === selectedPerturbation.toLowerCase());
         });
     });
 
@@ -157,7 +184,7 @@ const TaskGraph = () => {
                 <Options onPerturbationChange={setSelectedPerturbation}/>
                 <div className={'justify-center mt-7 float-start w-full'}>
                     <p> Graded Essays in Total </p>
-                    <p className={'text-4xl font-serif'}> {tests.length} </p>
+                    <p className={'text-4xl font-serif'}> {totalTests} </p>
                 </div>
             </div>
             <div className={'w-full h-52'}>
@@ -165,7 +192,7 @@ const TaskGraph = () => {
             </div>
             <div className={'w-full h-44'}>
                 <Bar data={data_criteria} options={
-                    createOptions(`Acceptable Tests by ${selectedPerturbation[0].toUpperCase() + selectedPerturbation.slice(1).toLowerCase()}`)}> </Bar>
+                    createOptions(`Tests by Grade: ${selectedPerturbation[0].toUpperCase() + selectedPerturbation.slice(1).toLowerCase()}`)}> </Bar>
             </div>
             <div className={'w-full h-96'}>
                 <Bar data={data_perturbations} options={createOptions("Tests by Criteria")}> </Bar>
