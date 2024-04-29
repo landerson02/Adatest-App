8a67b1767e0441eea1f6b0fe7db4491b58887124
Pranav Poddutoori <pranav.poddutoori@gmail.com>
2024-04-29 08:01:01 -0500

    Some changes to hopefully make gpu work

--- docker-gpu.sh
+++ docker-gpu.sh
@@ -1,1 +1,2 @@
-sudo docker build -t ada_backend:latest . --build-arg MODEL=$MODEL --build-arg HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN --build-arg OPENAI_API_KEY=$OPENAI_API_KEY
+source .env
+sudo docker build -t ada_backend:latest ./backend --build-arg MODEL=$MODEL --build-arg HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN --build-arg OPENAI_API_KEY=$OPENAI_API_KEY
