3ede91d1e4bf317a822a82e8e579112ff0bce93c
Pranav Poddutoori <pranav.poddutoori@gmail.com>
2024-04-29 08:34:58 -0500

    Some changes to hopefully make gpu work

--- docker-gpu.sh
+++ docker-gpu.sh
@@ -1,5 +1,5 @@
 source .env
 sudo chown -R ubuntu /etc/docker/daemon.json
-cp daemon.json /etc/docker/daemon.json
+sudo cp daemon.json /etc/docker/daemon.json
 sudo systemctl restart docker
 sudo docker build -t ada_backend:latest ./backend --build-arg MODEL=$MODEL --build-arg HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN --build-arg OPENAI_API_KEY=$OPENAI_API_KEY
