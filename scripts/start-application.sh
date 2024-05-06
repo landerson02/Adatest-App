b630a7ab4286ae562de7f503416a51f4a79d7600
Pranav <ppoddutoori@wisc.edu>
2024-05-06 15:09:37 -0500

    Changes for scripts JUST FOR VM, DON'T MERGE TO MAIN

--- scripts/start-application.sh
+++ scripts/start-application.sh
@@ -1,5 +1,10 @@
+# Updates source environment vars
+source ~/.bashrc
+
+# Run frontend
 cd ../frontend || return
 npm run dev &
 
+# Run backend
 cd ../backend || return
 python3 manage.py runserver &
--- scripts/stop-application.sh
+++ scripts/stop-application.sh
@@ -1,2 +1,8 @@
+# Updates source environment vars
+source ~/.bashrc
+
+# Kill frontend
 npx kill-port 3000
+
+# Kill backend
 lsof -t -i tcp:8000 | xargs kill -9
