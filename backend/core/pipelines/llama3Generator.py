874a507cead0387ef9ba126def07218a9d534aa6
Nora Tseng <nora@tseng.us>
2024-07-21 17:32:46 -0700

    dialect prompt

--- backend/core/pipelines/llama3Generator.py
+++ backend/core/pipelines/llama3Generator.py
@@ -115,7 +115,7 @@ class LlamaGeneratorPipeline(Pipeline):
               + "replace only nouns or verbs to the other language. Here is the sentence: ",
           'word_wall':"Identify the theme in this sentence that can produce a word wall:",
           'sentence_building': "Build on this sentence with increasing grammatical complexity: ",
-          'dialect': "Produce a Spanish dialectal difference for the following sentence. "
+          'dialect': "Produce a different Spanish dialect for the following sentence. "
               + "If the sentence is English, return 'not translating': "
         }
 
