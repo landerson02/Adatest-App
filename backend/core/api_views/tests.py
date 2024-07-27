8148fdf3135a2a8974df73b6f024187da1bce91c
Nora Tseng <nora@tseng.us>
2024-07-27 16:43:26 -0700

    prompt change

--- backend/core/api_views/tests.py
+++ backend/core/api_views/tests.py
@@ -199,8 +199,8 @@ def test_clear(request, config):
     df_map.clear()
 
     # reset grader prompts
-    grader_prompts['CU0'] = 'Does the following sentence include an acceptable/unacceptable description of the physics concept: Greater height means greater potential energy? Here is the sentence:'
-    grader_prompts['CU5'] = 'Does the following sentence include an acceptable/unacceptable description of the physics concept: Greater mass means greater energy? Here is the sentence:'
+    grader_prompts['CU0_esp'] = 'Does the following sentence include an acceptable/unacceptable description of the physics concept: Greater height means greater potential energy? Here is the sentence:'
+    grader_prompts['CU5_esp'] = 'Does the following sentence include an acceptable/unacceptable description of the physics concept: Greater mass means greater energy? Here is the sentence:'
     grader_prompts['Food'] = 'Does this sentence include an acceptable or unacceptable description of food and/or culture? Here is the sentence:'
 
     # reset grader pipelines
