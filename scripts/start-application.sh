a1ccf119aa84c3a7be7b6ef5406795a818beb651
Aayush <abbharadwaj@wisc.edu>
2025-02-17 14:27:35 -0600

    ngrok script

--- scripts/start-application.sh
+++ scripts/start-application.sh
@@ -5,3 +5,5 @@ npm run dev &
 # Run backend
 cd ../backend || return
 python3 manage.py runserver &
+
+ngrok http --url=true-directly-mollusk.ngrok-free.app localhost:8000
--- scripts/vm-setup.sh
+++ scripts/vm-setup.sh
@@ -34,6 +34,16 @@ pip install jinja2==3.1.0
 pip install --upgrade huggingface_hub
 huggingface-cli login --token $LOGIN_TOKEN --add-to-git-credential
 
+# Install ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
+
+ngrok config add-authtoken $NGROK_TOKEN
+
 # Updates project
 git pull
 
